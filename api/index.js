4a0c10c0ece2e69d08a15c2075d504ed9097c182
Randy Leon <rcharinwolf@gmail.com>
2024-02-27 17:17:07 -0300

    haciendo la funcionalidad del login / register.

--- api/index.js
+++ api/index.js
@@ -1,15 +1,56 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 
-app.use(cors());
-app.use(express.json())
+const salt = bcrypt.genSaltSync(10)
+const secret = 'hjscjdschSIPHCdsjcsd51612346';
 
-app.post('/register', (req, res) => {
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect('mongodb+srv://rcharinwolf:rcharinga401981@cluster0.eap27ag.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req, res) => {
     const {username, password} = req.body
-    res.json({requestData:{username, password}});
+    try {
+        const userDoc = await User.create({ 
+            username, 
+            password: bcrypt.hashSync( password, salt )
+        });
+        res.json(userDoc)        
+    } catch (e) {
+        res.status(400).json(e);
+    }
 });
+
+app.post('/login', async (req, res) =>{
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({ username, id:userDoc._id }, secret, {}, (err,token) => {
+            if(err) throw err;
+            res.cookie('token', token).json('ok');
+
+        })
+    } else {
+        res.status(400).json('Wrong Password')
+    } 
+});
+
+app.get('/profile', (req,res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err
+        res.json(info)
+    })  
+})
 
 app.listen(4000)
 
-//mongodb+srv://charinwolf:<password>@cluster0.wjzwhjp.mongodb.net/?retryWrites=true&w=majority
--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -5,14 +5,18 @@ const Register = () => {
   const [password, setPassword] = useState('');
   
   const register = async (e) => {
-    e.preventDefault();
-    await fetch('http://localhost:4000/register', {
+    e.preventDefault();    
+    const response = await fetch('http://localhost:4000/register', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: {'Content-Type':'application/json'}
-    })
+      })
+      if(response.status === 200){
+        alert('Registration Successesful')
+      }else{
+        alert('Registration Failed')
+      }
   }  
-
 
   return (
     <form className='register' onSubmit={register}> 
